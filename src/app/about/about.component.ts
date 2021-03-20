573d748f4c36beefc9d133a87f3d912de730376a
priyankagandham23 <priyanka.gandham@nirmaan.org>
2021-03-20 13:44:04 +0530

    created

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'b46',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
